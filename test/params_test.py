dc921e875ada3c1de6efc3b60f7b2f1e31f233ea
maayane <maayane.soumagnac@gmail.com>
2019-06-04 16:10:09 +0300

    updated test files

--- test/params_test.py
+++ test/params_test.py
@@ -4,9 +4,9 @@ mcmc=False# if False, plot with linear fit. If True, plots with mcmc
 
 ########## Definition of the output file ##########
 
-output_file_mcmc='./results_fit_sed_mcmc'
-output_file_linear='./result_fit_sed_mat'
-output_file_interpolation='./results_interpolation'
+output_file_mcmc='./test/results_fit_sed_mcmc'
+output_file_linear='./test/result_fit_sed_mat'
+output_file_interpolation='./test/results_interpolation'
 if mcmc==True:
     output=output_file_mcmc
 else:
@@ -22,13 +22,15 @@ z =0
 distance_modulus=33.51
 explosion_date=0 #in principle, this has to be in jd. For the test data it is in jd-explosion dates
 EBV=0.035
+csm=False
+
 data_file='./data_files/data_13dqy_formatted_for_package.txt' #must have at least the following fiels, and a header with the fields written as such: jd,flux,fluxerr,filter.
 # filter can be one of the following: ('UVW1','UVW2','UVM2','u_swift','v_swift','b_swift','g_sdss','r_sdss','i_sdss','z_sdss'
 # ,'r_cousin','i_cousin','h_2mass','j_2mass','u_johnson','b_johnson','v_johnson')
 dates_file='./data_files/13dqy_int_dates.txt'
 
 lower_limit_on_flux=1e-40
-filters_directory='/Users/maayanesoumagnac/AstroCosmo/GitHub_repositories/PhotoFit/PhotoFit/Filters' #put the path to the Filters directory here
+filters_directory='./PhotoFit/Filters' #put the path to the Filters directory here
 
 # Interpolation step
 already_run_interp_errors=dict() #don't touch this line
@@ -64,3 +66,6 @@ num_steps=350
 
 # In case you want to compare your R and T results with existing results from a file
 data_compare='./data_files/Yaron_2017_results.txt'#file with column#1: time from explosion, column#2: temperature (K), column#3:radius (cm)
+
+
+
