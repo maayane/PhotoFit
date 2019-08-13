cbd38a56615d81316af7f9f560eabd06f5eae0ff
maayane <maayane.soumagnac@gmail.com>
2019-08-12 17:48:43 -0700

    version 0.0.14

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='PhotoFit',
-    version='0.0.13',
+    version='0.0.14',
     #description=''
     #long_description=long_description,
     #long_description_content_type='text/markdown',    
